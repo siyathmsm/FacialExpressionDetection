31878690331656a3e4e19b4066db89cad87293df
Siyath MSM <87572745+siyathmsm@users.noreply.github.com>
2025-05-26 11:00:19 +0530

    Delete FrontEnd/templates directory

--- FrontEnd/templates/static/broadcast_API_lec.js
+++
@@ -1,8 +0,0 @@
-const channel = new BroadcastChannel('activity_channel');
-        document.querySelectorAll('.link').forEach(link => {
-            link.addEventListener('click', function(event) {
-                event.preventDefault();
-                const url = this.getAttribute('data-url');
-                channel.postMessage({ type: 'navigate', url: url });
-            });
-        });
